5014182b898e366ce059d35867e9148ae4473fe4
itggot-julia-ekeblad <38528613+itggot-julia-ekeblad@users.noreply.github.com>
2018-04-26 14:37:09 +0200

    sum to

---
+++ lib/sum_to.rb
@@ -0,0 +1,20 @@
+# Public: Adds together all the numbers up to your number.
+#
+# num  - Your desired number, an Integer.
+#
+# Examples
+#
+#   sum_to(5)
+#   # => 15
+#
+# Returns the added together numbers.
+
+def sum_to(num)
+    i = num
+    output = 0
+    while i > 0
+        output += i
+        i -= 1
+    end
+    return output
+end
