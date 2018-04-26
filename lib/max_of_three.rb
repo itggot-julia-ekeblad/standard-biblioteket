b83a1b108b317a7409511383a2cfef1f16dd8041
itggot-julia-ekeblad <38528613+itggot-julia-ekeblad@users.noreply.github.com>
2018-04-26 14:31:32 +0200

    max of three

---
+++ lib/max_of_three.rb
@@ -0,0 +1,22 @@
+# Public: Returns the biggest of your numbers.
+#
+# num1 - Your desired number, an Integer.
+# num2 - Your desired number, an Integer.
+# num3 - Your desired number, an Integer.
+#
+#   max_of_three(-7, 7, 3)
+#   # => 7
+#
+# Returns the biggest of your numbers.
+
+def max_of_three(num1, num2, num3)
+    output = 0
+    if num1 > num2 and num1 > num3
+        output = num1
+    elsif num2 > num3 and num2 > num1
+        output = num2
+    else
+        output = num3
+    end
+    return output
+end
