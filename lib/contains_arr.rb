ebee8d0ac50c48f5b6b79156875df305418618b9
itggot-julia-ekeblad <38528613+itggot-julia-ekeblad@users.noreply.github.com>
2018-05-03 14:08:29 +0200

    contains arr

--- lib/contains_arr.rb
+++ lib/contains_arr.rb
@@ -1,4 +1,4 @@
-# Public: Checks your letter exists in the array.
+# Public: Checks your character exists in the array.
 #
 # arr  - Your array with desierd continent.
 # char - A character of your choice.
@@ -8,7 +8,7 @@
 #   contains_arr(["bosse", "olof", "kalle", "olof"], "kurt")
 #   # => false
 #
-# Returns true if the letter exist, otherwise false.
+# Returns true if the character exist, otherwise false.
 def contains_arr(arr, char)
     i = 0
     while i < arr.length
