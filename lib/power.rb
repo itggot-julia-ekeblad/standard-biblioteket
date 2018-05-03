b79145b6ba19274728b2592e1ef09529bc0665d8
itggot-julia-ekeblad <38528613+itggot-julia-ekeblad@users.noreply.github.com>
2018-05-03 14:30:54 +0200

    upgraded power

--- lib/power.rb
+++ lib/power.rb
@@ -10,5 +10,11 @@
 #
 # Returns the added together numbers.
 def power(base, exponent)
-    return base ** exponent
+    i = 0
+    output = 1
+    while i < exponent
+        output *= base
+        i += 1
+    end
+    return output
 end
